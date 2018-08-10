1decbc1cf2423a06aac9b031e944ee8dcc4a82dc
Olga Kroshkina <okroshkina@axon.com>
2018-08-10 11:11:19 -0700

    Added build.sh for TeamCity build

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [[ -z $BUILD_NUMBER ]]; then
+  echo 'BUILD_NUMBER not defined. Defaulting to 0!' >&2
+  BUILD_NUMBER=0
+fi
+
+if [[ -z $BRANCH_NAME ]]; then
+  echo 'BRANCH_NAME not defined. Defaulting to "master"!' >&2
+  BRANCH_NAME="master"
+fi
+
+if [ $BRANCH_NAME == "master" ];
+then
+  export BUILD_QUALIFIER=""
+else
+  export BUILD_QUALIFIER="-$BRANCH_NAME-SNAPSHOT"
+fi
+
+# OK, now build Heimdall.
+
+./bin/sbt compile test package universal:packageBin -Dpackaging.buildQualifier="$BUILD_QUALIFIER" -Dpackaging.buildNumber="$BUILD_NUMBER"
+
