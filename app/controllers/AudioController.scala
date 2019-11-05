a86c69fb8a8f1482069431c23f300e0acad245e5
Hau Ma <hauma@axon.com>
2019-11-05 16:22:20 +0700

    update syntax

--- app/controllers/AudioController.scala
+++ app/controllers/AudioController.scala
@@ -10,10 +10,10 @@ import actions.{
 import com.typesafe.config.Config
 import javax.inject.Inject
 import models.hls.HlsManifestFormatter
+import models.common.PermissionType
 import play.api.libs.ws.WSResponse
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
 import services.rtm.{RtmClient, RtmResponseHandler}
-import models.common.PermissionType
 
 import scala.concurrent.ExecutionContext
 
