af1b18e50c30cc15eafa78e9777f6f397c51950a
Dang Le <dangnle@axon.com>
2020-05-13 16:21:30 +0700

    Keep streaming pipe open when streaming large file (#112)

--- app/services/document/DocumentClient.scala
+++ app/services/document/DocumentClient.scala
@@ -3,9 +3,11 @@ package services.document
 import com.evidence.service.common.logging.LazyLogging
 import com.typesafe.config.Config
 import java.net.URL
+
 import javax.inject.{Inject, Singleton}
 import play.api.libs.ws.{WSClient, WSResponse}
 
+import scala.concurrent.duration.Duration
 import scala.concurrent.{ExecutionContext, Future}
 
 trait DocumentClient {
@@ -18,7 +20,10 @@ class DocumentClientImpl @Inject()(config: Config, ws: WSClient)(implicit ex: Ex
     with LazyLogging {
 
   def view(presignedURL: URL): Future[WSResponse] = {
-    ws.url(presignedURL.toString).stream()
+    ws.url(presignedURL.toString)
+      .withMethod("GET")
+      .withRequestTimeout(Duration.Inf)
+      .stream()
   }
 
 }
