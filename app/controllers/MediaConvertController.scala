4076a8223ba2e38fe1d259c17471492069900388
Tin Nguyen <tinnguyen@axon.com>
2020-05-15 08:50:00 +0000

    Introduce flag for disable apidae at Heimdall side (#114)

--- app/controllers/MediaConvertController.scala
+++ app/controllers/MediaConvertController.scala
@@ -3,9 +3,11 @@ package controllers
 import actions._
 import akka.util.ByteString
 import com.evidence.service.common.logging.LazyLogging
+import com.typesafe.config.Config
 import javax.inject.Inject
 import models.common.{AuthorizationAttr, PermissionType}
 import play.api.http.{ContentTypes, HttpEntity}
+import play.api.libs.json.Json
 import play.api.libs.ws.WSResponse
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, ResponseHeader, Result}
 
@@ -21,6 +23,7 @@ class MediaConvertController @Inject()(
                                         apidaeRequestAction: ApidaeRequestAction,
                                         apidae: ApidaeClient,
                                         audit: AuditClient,
+                                        config: Config,
                                         components: ControllerComponents)(implicit ex: ExecutionContext)
     extends AbstractController(components)
     with LazyLogging
@@ -33,16 +36,21 @@ class MediaConvertController @Inject()(
         andThen permValidation.build(PermissionType.View)
         andThen apidaeRequestAction
     ).async { implicit request =>
-      val authHandler = request.attrs(AuthorizationAttr.Key)
+      val isApidaeEnable = config.getBoolean("edc.service.apidae.enable")
+      if (!isApidaeEnable) {
+        Future.successful(NotImplemented(Json.obj("message" -> "This function is under-construction for your region")))
+      } else {
+        val authHandler = request.attrs(AuthorizationAttr.Key)
 
-      audit.recordEndSuccess(
+        audit.recordEndSuccess(
           EvidencePlaybackRequested(
             evidenceTid(request.file.evidenceId, request.file.partnerId),
             updatedByTid(authHandler.parsedJwt),
             fileTid(request.file.fileId, request.file.partnerId),
             request.request.clientIpAddress))
 
-      apidae.transcode(request.file.partnerId, request.userId, request.file.evidenceId, request.file.fileId)
+        apidae.transcode(request.file.partnerId, request.userId, request.file.evidenceId, request.file.fileId)
+      }
     }
 
   def status: Action[AnyContent] =
@@ -51,7 +59,12 @@ class MediaConvertController @Inject()(
         andThen permValidation.build(PermissionType.View)
         andThen apidaeRequestAction
     ).async { implicit request =>
+      val isApidaeEnable = config.getBoolean("edc.service.apidae.enable")
+      if (!isApidaeEnable) {
+        Future.successful(NotImplemented(Json.obj("message" -> "This function is under-construction for your region")))
+      } else {
         apidae.getTranscodingStatus(request.file.partnerId, request.userId, request.file.evidenceId, request.file.fileId)
+      }
     }
 
   implicit def Response2Result(response: Future[WSResponse]): Future[Result] = {
