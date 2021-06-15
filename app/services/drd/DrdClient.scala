5a2f77b3fb972a3140eaca030846fef08b1b4827
Luu Nguyen <lunguyen@axon.com>
2021-06-15 13:41:11 +0700

    update header key: Client-Ip

--- app/services/drd/DrdClient.scala
+++ app/services/drd/DrdClient.scala
@@ -12,7 +12,14 @@ import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, Future}
 
 trait DrdClient {
-  def call(endpoint: String, method: String, partnerId: UUID, userId: UUID, body: Option[JsValue], clientIpAddress: String): Future[WSResponse]
+
+  def call(
+    endpoint: String,
+    method: String,
+    partnerId: UUID,
+    userId: UUID,
+    body: Option[JsValue],
+    clientIpAddress: String): Future[WSResponse]
 }
 
 @Singleton
@@ -20,12 +27,18 @@ class DrdClientImpl @Inject()(config: Config, ws: WSClient)(implicit ex: Executi
     extends DrdClient
     with LazyLogging {
 
-  def call(endpoint: String, method: String, partnerId: UUID, userId: UUID, body: Option[JsValue], clientIpAddress: String) = {
+  def call(
+    endpoint: String,
+    method: String,
+    partnerId: UUID,
+    userId: UUID,
+    body: Option[JsValue],
+    clientIpAddress: String) = {
     ws.url(config.getString("edc.service.drd.host") + endpoint)
       .addHttpHeaders(
         "Partner-Id"            -> partnerId.toString,
         "Authenticated-User-Id" -> userId.toString,
-        "X-Forwarded-For" -> clientIpAddress,
+        "Client-Ip"             -> clientIpAddress,
         "Accept"                -> "application/json",
       )
       .withMethod(method)
