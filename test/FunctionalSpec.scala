ea3aa26f1f9a8cfeca1c2ecd065eb43a409a9979
Dong Nguyen <dongnguyen@axon.com>
2021-08-14 17:21:37 +0700

    Revert "skip test for quick test other blocker"
    This reverts commit ca0baaf519cf657950090e97f2a297cf745145dd.

--- test/FunctionalSpec.scala
+++ test/FunctionalSpec.scala
@@ -7,26 +7,26 @@ import play.api.test.Helpers._
   * Functional tests start a Play application internally, available
   * as `app`.
   */
-//class FunctionalSpec extends PlaySpec with GuiceOneAppPerSuite {
-//
-//  "Routes" must {
-//
-//    "send 404 on a non-existing route" in {
-//      route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
-//    }
-//
-//    "send 403 on a HLS bad request - missing streaming token" in {
-//      route(app, FakeRequest(GET, "/media/hls/segment")).map(status(_)) mustBe Some(FORBIDDEN)
-//    }
-//
-//    "send 403 on a HLS long segment bad request - missing streaming token" in {
-//      route(app, FakeRequest(GET, "/media/hls/longsegment")).map(status(_)) mustBe Some(FORBIDDEN)
-//    }
-//
-//
-//    "send 200 on a good request" in {
-//      route(app, FakeRequest(GET, "/media/alive")).map(status(_)) mustBe Some(OK)
-//    }
-//
-//  }
-//}
+class FunctionalSpec extends PlaySpec with GuiceOneAppPerSuite {
+
+  "Routes" must {
+
+    "send 404 on a non-existing route" in {
+      route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
+    }
+
+    "send 403 on a HLS bad request - missing streaming token" in {
+      route(app, FakeRequest(GET, "/media/hls/segment")).map(status(_)) mustBe Some(FORBIDDEN)
+    }
+
+    "send 403 on a HLS long segment bad request - missing streaming token" in {
+      route(app, FakeRequest(GET, "/media/hls/longsegment")).map(status(_)) mustBe Some(FORBIDDEN)
+    }
+
+
+    "send 200 on a good request" in {
+      route(app, FakeRequest(GET, "/media/alive")).map(status(_)) mustBe Some(OK)
+    }
+
+  }
+}
