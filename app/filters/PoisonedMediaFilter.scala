caedb152bf5e6c285c226e7b84c74a48b796e232
Dang Le <dangnle@axon.com>
2021-08-19 12:14:25 +0700

    refactor PoisonEvidenceFilter (#262)

--- app/filters/PoisonedMediaFilter.scala
+++ app/filters/PoisonedMediaFilter.scala
@@ -4,29 +4,27 @@ import java.util.UUID
 import akka.stream.Materializer
 import com.typesafe.config.Config
 import javax.inject.Inject
+import models.common.MediaIdentAttr
 import play.api.mvc.{Filter, RequestHeader, Result, Results}
 import utils.UUIDHelper
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class PoisonedEvidenceFilter @Inject()(implicit val mat: Materializer, ec: ExecutionContext, config: Config)
+class PoisonedMediaFilter @Inject()(implicit val mat: Materializer, ec: ExecutionContext, config: Config)
   extends Filter
     with UUIDHelper {
 
   final val poisonedEvidenceList = config.getStringList("heimdall.poisoned_evidences")
 
-
   def apply(
              nextFilter: RequestHeader => Future[Result]
            )(requestHeader: RequestHeader): Future[Result] = {
-
-    getUuidListByKey("evidence_id", requestHeader.queryString) match {
-      case Some(evidenceIds) =>
-        if (filterPoisonedEvidence(evidenceIds)) {
+    requestHeader.attrs.get(MediaIdentAttr.Key) match {
+      case Some(mediaIdent) =>
+        if (filterPoisonedEvidence(mediaIdent.evidenceIds))
           Future.successful(Results.BadRequest)
-        } else {
+        else
           nextFilter(requestHeader)
-        }
       case _ => nextFilter(requestHeader)
     }
   }
