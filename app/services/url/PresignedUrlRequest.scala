e800dc2e47d1ee3433967e09767d2ee1fbe2f0b6
Elly Tran <thutran@axon.com>
2022-07-06 14:45:37 +0700

    Rebase PR 314

--- app/services/url/PresignedUrlRequest.scala
+++ app/services/url/PresignedUrlRequest.scala
@@ -14,11 +14,31 @@ import javax.inject.Inject
 
 case class PresignedUrlRequest @Inject()(sage: SageClient, dredd: DreddClient)(implicit executionContext: ExecutionContext) extends LazyLogging{
   def getUrl[A](file: FileIdent, request: HeimdallRequest[A], ttl: Duration = HdlTtl.urlExpired): Future[URL] = {
-    getUrlfromDredd(file, request, ttl)
+    getUrlTest(file, request, ttl)
+  }
+
+  private def getUrlTest[A](file: FileIdent, request: HeimdallRequest[A], ttl: Duration): Future[URL] = {
+    val sageResFuture = getUrlfromSage(file, ttl)
+    val dreddResFuture = getUrlfromDredd(file, request, ttl)
+
+    for {
+      sageRes <- sageResFuture
+      dreddRes <- dreddResFuture
+    } yield dreddRes
   }
 
   // Internal get-url logic
   private def getUrlfromDredd[A](file: FileIdent, request: HeimdallRequest[A], ttl: Duration) : Future[URL] = {
     dredd.getUrl(file, request, ttl)
+  }
+
+  private def getUrlfromSage(file: FileIdent, ttl: Duration): Future[URL] = {
+    sage.getUrl(file, ttl).flatMap(_.fold(l => {
+      val mes = l.message
+      logger.debug("Sage fails to return URL ")("error" -> mes, "errorCode" -> l.errorCode)
+      Future.failed( new Exception("Sage fails to return URL " + s"error=$mes"))
+    },
+      r => Future.successful(r))
+    )
   }
 }
