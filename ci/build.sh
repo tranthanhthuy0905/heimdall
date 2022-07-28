a2b8806a9967b974419bec1eaee620809e0e29b5
Elly Tran <thutran@axon.com>
2022-07-28 17:29:33 +0700

    Fix Docker push fail to hub

--- ci/build.sh
+++ ci/build.sh
@@ -3,13 +3,24 @@
 set -exu
 
 #------------------------ Setting docker image version --------------------
+SERVICE="heimdall"
 dateStr=$(date "+%Y%m%d")
 version=$dateStr.$BUILD_NUMBER
 release_branch_regex='^refs/heads/releases/(release-2[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2})$'
 
+DOCKER_IMAGE_NAME="ecom/${SERVICE}"
+DOCKER_IMAGE_VERSION="$dateStr.$BUILD_NUMBER"
+DOCKER_IMAGE_TAG="$DOCKER_IMAGE_NAME:$DOCKER_IMAGE_VERSION"
+
 if [[ $BRANCH_NAME =~ $release_branch_regex ]]; then
   version="$version-${BASH_REMATCH[1]}"
 fi
+
+# Output docker images and tag to script
+echo ${DOCKER_IMAGE_TAG} >>docker-images.txt
+
+# Let Argocd update job
+echo "##teamcity[setParameter name='docker.image_tag' value='${version}']"
 
 ./tests.sh
 
