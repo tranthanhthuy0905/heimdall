25c3bc139c4e031950ed0a7587ab56ad9ee8e5d0
Neil Nguyen <nenguyen@axon.com>
2022-08-11 11:03:16 +0700

    update

--- app/controllers/ConvertedFilesController.scala
+++ app/controllers/ConvertedFilesController.scala
@@ -1,31 +1,33 @@
 package controllers
 
 import com.axon.sage.protos.v1.evidence_video_service.ConvertedFile
-import actions.{ConvertedFilesRequestAction, FeatureValidationActionBuilder, HeimdallRequestAction, PermValidationActionBuilder, TokenValidationAction}
+import actions.{ApidaeRequestAction, HeimdallRequestAction, PermValidationActionBuilder, TokenValidationAction}
 import com.evidence.service.common.logging.LazyLogging
+import com.evidence.service.common.monad.FutureEither
 import models.common.{AuthorizationAttr, PermissionType}
+import play.api.http.ContentTypes
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
-import services.sage.{SageJson, SageClient}
-
+import services.sage.{SageClient, SageJson}
+import utils.HdlResponseHelpers
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
 
 class ConvertedFilesController @Inject()(heimdallRequestAction: HeimdallRequestAction,
                                          tokenValidationAction: TokenValidationAction,
                                          permValidation: PermValidationActionBuilder,
-                                         featureValidationAction: FeatureValidationActionBuilder,
-                                         convertedFilesRequestAction: ConvertedFilesRequestAction,
+                                         apidaeRequestAction: ApidaeRequestAction,
                                          sage: SageClient,
                                          components: ControllerComponents)(implicit ex: ExecutionContext)
   extends AbstractController(components)
     with SageJson
-    with LazyLogging {
+    with LazyLogging
+    with HdlResponseHelpers  {
 
   def getConvertedFiles: Action[AnyContent] =
     (
       heimdallRequestAction
         andThen permValidation.build(PermissionType.View)
-        andThen convertedFilesRequestAction
+        andThen apidaeRequestAction
       ).async { implicit request =>
 
       def filterExtractionInfo(id: services.sage.EvidenceId, audienceId: String, files: Seq[ConvertedFile]): Seq[ConvertedFile] = {
@@ -35,12 +37,11 @@ class ConvertedFilesController @Inject()(heimdallRequestAction: HeimdallRequestA
       val authHandler = request.attrs(AuthorizationAttr.Key)
       val evidenceId = services.sage.EvidenceId(request.file.evidenceId, request.file.partnerId)
 
-      sage.
-        getConvertedFiles(services.sage.EvidenceId(request.file.evidenceId, request.file.partnerId)).
-        map {
-          case Right(files) => Ok(convertedFilesToJsonValue(filterExtractionInfo(evidenceId, authHandler.parsedJwt.audienceId, files)))
-          case Left(error) =>
-            InternalServerError(error.message)
-        }
+      (for {
+        result <- FutureEither(
+          sage.
+            getConvertedFiles(services.sage.EvidenceId(request.file.evidenceId, request.file.partnerId))
+        ).mapLeft(toHttpStatus("failedToSendEvidenceConvertedFilesEvent")(_))
+      } yield result).fold(error, files => Ok(convertedFilesToJsonValue(filterExtractionInfo(evidenceId, authHandler.parsedJwt.audienceId, files))).as(ContentTypes.JSON))
     }
 }
--- app/services/sage/ConvertedFilesRequest.scala
+++
@@ -1,8 +0,0 @@
-package services.sage
-
-import models.common.{FileIdent, HeimdallRequest}
-
-import play.api.mvc.WrappedRequest
-
-case class ConvertedFilesRequest[A](file: FileIdent, request: HeimdallRequest[A])
-  extends WrappedRequest[A](request)
