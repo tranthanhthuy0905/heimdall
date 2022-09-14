b39ef5c8b3d2705ff45f4e42c5ca2668346817f0
Elly Tran <thutran@axon.com>
2022-09-14 21:35:00 +0700

    Test header Ag1

--- app/actions/HeimdallRequestAction.scala
+++ app/actions/HeimdallRequestAction.scala
@@ -1,5 +1,7 @@
 package actions
 
+import com.evidence.service.common.logging.LazyLogging
+
 import javax.inject.Inject
 import models.common.{AuthorizationAttr, HeimdallRequest}
 import play.api.mvc._
@@ -9,9 +11,11 @@ import scala.concurrent.{ExecutionContext, Future}
 case class HeimdallRequestAction @Inject()(defaultParser: BodyParsers.Default)(
   implicit val ex: ExecutionContext
 ) extends ActionBuilder[HeimdallRequest, AnyContent]
-    with ActionRefiner[Request, HeimdallRequest] {
+    with ActionRefiner[Request, HeimdallRequest]
+    with LazyLogging {
 
   def refine[A](request: Request[A]): Future[Either[Result, HeimdallRequest[A]]] = {
+    logger.info("Request header")("header" -> request.queryString)
     Future(
       request.attrs
         .get(AuthorizationAttr.Key)
